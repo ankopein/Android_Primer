6092b2df927d4721a4102dd76d5fd4b65d73f6ce
ankopein <ankopein@edu.aau.at>
2020-03-15 13:33:51 +0100

    Add the user-interface for the main component
    button and label strings are defined in resource file strings.xml
    for simple and quick editing.

---
+++ README.md
@@ -0,0 +1,4 @@
+## Simple Java Android app 
+
+User enters a number in an editText field and can choose to do some local calculation and display the result in a textView or request a server and display the response in the same textView.
+Local calculation is to show only the prime digits of the entered number.
--- app/src/main/java/com/example/se2_single/MainActivity.java
+++ app/src/main/java/com/example/se2_single/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.se2_single;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtnLocalCalc(View view) {
+
+    }
+
+    public void onClickBtnSendRequest(View view) {
+
     }
 }
