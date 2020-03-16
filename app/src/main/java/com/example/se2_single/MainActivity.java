13b378984dd007886d6549977f78b9881bb00d37
ankopein <ankopein@edu.aau.at>
2020-03-16 10:12:39 +0100

    Fix missing access-specifier

--- app/src/main/java/com/example/se2_single/MainActivity.java
+++ app/src/main/java/com/example/se2_single/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_local = null;
     private TextView tv_result = null;
 
-    Disposable disposable = null;
+    private Disposable disposable = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
